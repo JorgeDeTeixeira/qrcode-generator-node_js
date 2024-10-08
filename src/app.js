9104b78ee382fbf2e554b86558a97efb4ae4344f
Jorge Teixeira <jorgedeteixeirademelo@gmail.com>
2024-10-08 17:06:57 -0300

    feat: Main import

--- src/app.js
+++ src/app.js
@@ -1,5 +1,15 @@
 import prompt from "prompt";
 
-async function main() {}
+import mainPrompt from "../src/prompts/prompt-main.js";
+
+async function main() {
+  prompt.get(mainPrompt, async (err, choice) => {
+    if (choice.select == 1) {
+      console.log("Opção 1");
+    } else if (choice.select == 2) {
+      console.log("Opção 2");
+    }
+  });
+}
 
 main();
