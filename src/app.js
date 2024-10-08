49da68d56b73f2c20b58ccbcd124421adeec634b
Jorge Teixeira <jorgedeteixeirademelo@gmail.com>
2024-10-08 17:23:21 -0300

    feat: Qr-Code Architeture

--- src/app.js
+++ src/app.js
@@ -1,13 +1,15 @@
 import prompt from "prompt";
+import chalk from "chalk";
 
 import mainPrompt from "../src/prompts/prompt-main.js";
+import createQRCode from "./services/qr-code/create.js";
 
 async function main() {
   prompt.get(mainPrompt, async (err, choice) => {
     if (choice.select == 1) {
-      console.log("Opção 1");
+      await createQRCode();
     } else if (choice.select == 2) {
-      console.log("Opção 2");
+      console.log(chalk.yellowBright.bold("Opção 2"));
     }
   });
 }
--- src/prompts/prompt-main.js
+++ src/prompts/prompt-main.js
@@ -1,9 +1,13 @@
+import chalk from "chalk";
+
 const mainPrompt = [
   {
     name: "select",
-    description: "Escolha a ferramenta (1 - QRCODE ou 2 - PASSWORD)",
+    description: chalk.blue.bold(
+      "Escolha a ferramenta (1 - QRCODE ou 2 - PASSWORD)"
+    ),
     pattern: /^[1-2]+$/,
-    message: "Escolha apenas entre 1 e 2!",
+    message: chalk.red.bold("Escolha apenas entre 1 e 2!"),
     required: true,
   },
 ];
